11697b647ca43a96bdb7e6cf768c624304355918
paul.xiao <paul.xiao@axel.org>
2019-06-21 11:17:26 +0800

    use docs

---
+++ docs/precache-manifest.d1176ce37de45c0178bbb6a6887a27c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef59f5ad85159eba2a0f799da2bba54",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "47baa31d670bee3addb3",
+    "url": "/react/static/css/main.c1559d27.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/react/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "47baa31d670bee3addb3",
+    "url": "/react/static/js/main.c1ba64f5.chunk.js"
+  },
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.2e87a795.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
