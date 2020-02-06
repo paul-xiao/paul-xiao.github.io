b953a065a251719e5daba3049a704d615aebf1e6
paul.xiao <paul.xiao@axel.org>
2020-02-06 18:09:09 +0800

    update theme

---
+++ .vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: `Paul's Blog`,
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    // logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Archives', link: '/archives' },
+      { text: 'Tags', link: '/tags' },
+      { text: 'About', link: '/about' }
+    ],
+    displayAllHeaders: true,
+    sidebar: 'auto'
+  }
+}
